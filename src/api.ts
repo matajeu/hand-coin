f3cdaf5c4c083bff25b7aa79b75a025a08267981
하돌 <bayajeu@gmail.com>
2022-10-22 11:21:16 +0900

    ApexChart를 이용한 최근 7일 coin price chart 구현
    Co-Authored-By: 나똥똥 <74886744+naddongddong@users.noreply.github.com>

--- src/api.ts
+++ src/api.ts
@@ -15,3 +15,11 @@ export const fetchCoinTickers = (coinId: string) => {
     response.json()
   );
 };
+
+export const fetchCoinHistory = (coinId: string) => {
+  // const endDate = Math.floor(Date.now() / 1000);
+  // const startDate = endDate - 60 * 60 * 24 * 7;
+  return fetch(
+    `https://ohlcv-api.nomadcoders.workers.dev/?coinId=${coinId}`
+  ).then((response) => response.json());
+};
--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,5 +1,97 @@
+import { useQuery } from "react-query";
+import { useOutletContext } from "react-router-dom";
+import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+
+interface IChartProps {
+  coinId: string;
+}
+
+interface IHistoricalData {
+  time_open: string;
+  time_close: number;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
+
 const Chart = () => {
-  return <h1>Chart</h1>;
+  const { coinId } = useOutletContext<IChartProps>();
+  const { isLoading, data } = useQuery<IHistoricalData[]>(
+    ["ohlcv", coinId],
+    () => fetchCoinHistory(coinId),
+    {
+      refetchInterval: 10000,
+    }
+  );
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart"
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: data?.map((price) => price.close) as number[],
+            },
+          ]}
+          options={{
+            chart: {
+              width: 500,
+              height: 500,
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+            stroke: {
+              curve: "smooth",
+              width: 3,
+            },
+            xaxis: {
+              labels: {
+                show: false,
+              },
+              axisTicks: {
+                show: false,
+              },
+              axisBorder: {
+                show: false,
+              },
+              type: "datetime",
+              categories: data?.map((price) =>
+                new Date(price.time_close * 1000).toISOString()
+              ),
+            },
+            yaxis: {
+              show: false,
+            },
+            fill: {
+              type: "gradient",
+              gradient: {
+                gradientToColors: ["#0be881"],
+                stops: [0, 100],
+              },
+            },
+            colors: ["#0fbcf9"],
+            tooltip: {
+              y: {
+                formatter: (value) => `$${value.toFixed(3)}`,
+              },
+            },
+            theme: {
+              mode: "dark",
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 };
 
 export default Chart;
