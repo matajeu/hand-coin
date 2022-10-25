0900893e4ae4d5ab9abf2326ff1c94e7b805c91e
하돌 <bayajeu@gmail.com>
2022-10-25 17:40:43 +0900

    docs: controller flex 수정
    Co-Authored-By: 나똥똥 <74886744+naddongddong@users.noreply.github.com>

--- src/components/Controller.tsx
+++ src/components/Controller.tsx
@@ -1,17 +1,26 @@
 import { Link } from "react-router-dom";
 import { useSetRecoilState } from "recoil";
 import { themeModeAtom } from "../atoms";
+import styled from "styled-components";
+
+const Container = styled.div`
+  position: relative;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  background-color: yellowgreen;
+`;
 
 const Controller = () => {
   const setDarkAtom = useSetRecoilState(themeModeAtom);
   const toggleSwitch = () => setDarkAtom((prev: boolean) => !prev);
   return (
-    <>
+    <Container>
       <button>
         <Link to="/">Main</Link>
       </button>
       <button onClick={toggleSwitch}>Toggle Mode</button>
-    </>
+    </Container>
   );
 };
 
--- src/pages/Coin.tsx
+++ src/pages/Coin.tsx
@@ -22,9 +22,10 @@ const Loader = styled.span`
 `;
 
 const Container = styled.div`
-  padding: 0px 20px;
+  padding: 0 20px;
   max-width: 480px;
   margin: 0 auto;
+  background-color: yellow;
 `;
 
 const Header = styled.header`
