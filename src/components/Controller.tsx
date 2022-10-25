20d832ea7bb2135fd1124b6137c04db378cfdd1c
하돌 <bayajeu@gmail.com>
2022-10-26 01:24:44 +0900

    컨트롤러 컴포넌트 UI 개선
    Co-Authored-By: 나똥똥 <74886744+naddongddong@users.noreply.github.com>

--- src/components/Controller.tsx
+++ src/components/Controller.tsx
@@ -1,25 +1,93 @@
-import { Link } from "react-router-dom";
-import { useSetRecoilState } from "recoil";
+import { Link, useLocation } from "react-router-dom";
+import { useRecoilState } from "recoil";
 import { themeModeAtom } from "../atoms";
 import styled from "styled-components";
+import { FaArrowLeft } from "react-icons/fa";
 
 const Container = styled.div`
   position: relative;
   width: 100%;
   display: flex;
   justify-content: space-between;
-  background-color: yellowgreen;
+  top: 10px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 3rem;
+`;
+
+const BackButton = styled(Wrapper)`
+  width: 3rem;
+  color: ${(props) => props.theme.bgColor};
+  background-color: ${(props) => props.theme.accentColor};
+  border: none;
+  border-radius: 50%;
+`;
+
+const ToggleBtn = styled(Wrapper)``;
+
+const ToggleInput = styled.input`
+  display: none;
+  &:checked + label {
+    background-color: ${(props) => props.theme.accentColor};
+  }
+  &:checked + label:after {
+    left: 50%;
+  }
+`;
+
+const ToggleLabel = styled.label`
+  display: inline-block;
+  width: 4em;
+  height: 2em;
+  background-color: ${(props) => props.theme.accentColor};
+  border-radius: 2em;
+  padding: 0.1em;
+  transition: all 0.4s ease;
+  cursor: pointer;
+  &:after {
+    display: block;
+    position: relative;
+    content: "";
+    width: 50%;
+    height: 100%;
+    left: 0%;
+    border-radius: 50%;
+    background-color: ${(props) => props.theme.bgColor};
+    transition: all 0.2s ease;
+  }
+  &:before {
+    display: none;
+  }
 `;
 
 const Controller = () => {
-  const setDarkAtom = useSetRecoilState(themeModeAtom);
+  const [darkAtom, setDarkAtom] = useRecoilState(themeModeAtom);
+  const location = useLocation();
   const toggleSwitch = () => setDarkAtom((prev: boolean) => !prev);
   return (
     <Container>
-      <button>
-        <Link to="/">Main</Link>
-      </button>
-      <button onClick={toggleSwitch}>Toggle Mode</button>
+      {location.pathname === "/" ? (
+        <Wrapper />
+      ) : (
+        <Link to="/">
+          <BackButton>
+            <FaArrowLeft />
+          </BackButton>
+        </Link>
+      )}
+      <ToggleBtn>
+        <ToggleInput
+          type="checkbox"
+          id="toggle"
+          checked={darkAtom}
+          onChange={toggleSwitch}
+        />
+        <ToggleLabel htmlFor="toggle" />
+      </ToggleBtn>
     </Container>
   );
 };
